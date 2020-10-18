e9b35bacf94a032c34fb90218ddb1449686c3f2a
Alexander Bowers <7319abowers@gmail.com>
2020-10-18 22:25:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,6 +6,12 @@ def game_hash
   game_hash[:home][:colors] = ["Black, White"]
   game_hash[:home][:players] = []
   game_hash[:home][:players][0] = {}
+    game_hash[:home][:players][0][:player_name] = "Alan Anderson"
+    game_hash[:home][:players][0][:number] = 0
+    game_hash[:home][:players][0][:shoe] = 16
+    game_hash[:home][:players][0][:points] = 22
+    game_hash[:home][:players][0][:rebounds] = 12
+    game_hash[:home][:players][0][]
   game_hash[:home][:players][1] = {}
   game_hash[:home][:players][2] = {}
   game_hash[:home][:players][3] = {}
@@ -16,6 +22,12 @@ def game_hash
   game_hash[:away][:team_name] = "Charlotte Hornets"
   game_hash[:away][:colors] = "Turquoise, Purple"
   game_hash[:away][:players] = []
+  game_hash[:away][:players][0] = {}
+  game_hash[:home][:players][1] = {}
+  game_hash[:home][:players][2] = {}
+  game_hash[:home][:players][3] = {}
+  game_hash[:home][:players][4] = {}
+
   game_hash
 end
 #top level has two keys, :home, and :array
