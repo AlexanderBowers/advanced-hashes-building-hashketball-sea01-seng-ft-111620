b34d8096e3aff2fa5880fda1467648616ae94482
Alexander Bowers <7319abowers@gmail.com>
2020-10-18 22:14:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
   game_hash[:home] = {}
   game_hash[:home][:team_name] = "Brooklyn Nets"
   game_hash[:home][:colors] = ["Black, White"]
-  game_hash[:home][:players] = []
+  #game_hash[:home][:players] = []
 
 end
 #top level has two keys, :home, and :array
