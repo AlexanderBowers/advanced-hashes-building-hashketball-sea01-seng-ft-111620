db5b28e2d4db83a6868ddfa453ba7f545ad4b932
Alexander Bowers <7319abowers@gmail.com>
2020-10-18 22:04:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,28 @@
 # Write your code here!
+def game_hash
+
+end
+#top level has two keys, :home, and :array
+=begin
+hash
+{
+:home => {
+  :team_name => "",
+  :colors =>  ["", "", ""],
+  :players =>  [
+    {:player_name => "", :number => "", :shoe => "", :points => "",
+    :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""}
+    ],
+  }
+
+               
+:away => {
+  :team_name => "",
+  :colors =>  ["", "", ""],
+  :players =>  [
+    {:player_name => "", :number => "", :shoe => "", :points => "",
+    :rebounds => "", :assists => "", :steals => "", :blocks => "", :slam_dunks => ""}
+    ],
+  }
+
+}
